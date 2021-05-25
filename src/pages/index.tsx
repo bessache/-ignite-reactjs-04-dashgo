871f71fc2aca11a3d593ec359768715988563bcf
Renato Bessa <bessache@outlook.com>
2021-05-25 14:09:12 -0300

    commit aula criando pagina de sign in

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,66 @@
-import Head from 'next/head'
+import {Flex, Input, Button, Stack, FormLabel, FormControl} from '@chakra-ui/react'
 
 
 export default function Home() {
   return (
-    <h1>DashGo</h1>
+    <Flex 
+      w="100vm" 
+      h="100vh" 
+      alignItems="center" 
+      justify="center"
+    >
+      <Flex 
+        as="form" 
+        width="100%" 
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius="8"
+        flexDirection="column"
+      >
+        <Stack spacing="4">
+          <FormControl>
+            <FormLabel htmlFor="email">E-mail</FormLabel>
+            <Input 
+              name="email"
+              id="email"
+              type="email"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: 'gray.900'
+              }}
+              size="lg"
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="password">Senha</FormLabel>
+            <Input 
+              name="password" 
+              id="password"
+              type="password"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: 'gray.900'
+              }}
+              size="lg"
+            />
+          </FormControl>
+        </Stack>
+
+        <Button 
+          type="submit" 
+          marginTop="6" 
+          colorScheme="pink"
+          size="lg"
+        >
+          Entrar
+        </Button>
+
+      </Flex>
+    </Flex>
   )
 }
