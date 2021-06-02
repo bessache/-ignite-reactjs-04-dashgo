2276ddb89cc46eb0ae3562202c0657664adca14b
Renato Bessa <bessache@outlook.com>
2021-06-02 12:05:26 -0300

    commit aula header responsivo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import {Flex} from '@chakra-ui/react'
+import {Flex, useBreakpointValue} from '@chakra-ui/react'
 
 import { Logo } from './Logo';
 import { NotificationNav } from './NotificationNav';
@@ -6,6 +6,11 @@ import { Profile } from './Profile'
 import { SearchBox } from './SearchBox';
 
 export function Header(){
+
+    const isWideVersion = useBreakpointValue({
+        base: false,
+        lg: true,
+    })
     return (
         <Flex
             w="100%" 
@@ -18,13 +23,13 @@ export function Header(){
             px="6"
         >
             <Logo/>
-            <SearchBox/>
+            {isWideVersion && <SearchBox/>}
             <Flex
                 align="center"
                 ml="auto"
             >
                 <NotificationNav/>
-                <Profile/>
+                <Profile showProfileData={isWideVersion}/>
             </Flex>
         </Flex>
 
