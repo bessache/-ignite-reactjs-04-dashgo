6deb96b45ecb41a5ad46d784b8ee59dec73aa2a9
Renato Bessa <bessache@outlook.com>
2021-06-20 12:18:23 -0300

    commit aula listagem de usuarios formatação de datas não funcional

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -16,10 +16,21 @@ export default function UserList() {
     const {data, isLoading, error} = useQuery('users', async () => {
         const response = await fetch('http://localhost:3000/api/users')
         const data = response.json()
+        // const users = data.users.map(user=>{
+        //     return {
+        //         id:user.id,
+        //         name:user.name,
+        //         email:user.email,
+        //         createdAt:new Date(user.createdAt).toLocaleString('pt-BR', {
+        //             day: '2-digit',
+        //             month: 'long',
+        //             year: 'numeric'
+        //         }),
+        //     }
+        // });
+        
         return data;
     })
-
-    
 
     const isWideVersion = useBreakpointValue({
         base: false,
@@ -71,77 +82,33 @@ export default function UserList() {
                                     </Tr>
                                 </Thead>
                                 <Tbody>
-                                    <Tr>
-                                        <Td px={["4", "4", "6"]}>
-                                            <Checkbox colorScheme="pink"/>
-                                        </Td>
-                                        <Td>
-                                            <Box>
-                                                <Text fontWeight="bold">Renato Bessa</Text>
-                                                <Text fontSize="sm" color="gray.300">bessache@outlook.com</Text>
-                                            </Box>
-                                        </Td>
-                                        {isWideVersion && <Td>04 de Abril, 2021</Td>}
-                                        <Td>
-                                            <Button 
-                                                as="a" 
-                                                size="sm" 
-                                                fontSize="sm" 
-                                                colorScheme="purple"
-                                                leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
-                                            >
-                                                {isWideVersion? 'Editar' : ''}
-                                            </Button>
-
-                                        </Td>
-                                    </Tr>
-                                    <Tr>
-                                        <Td px={["4", "4", "6"]}>
-                                            <Checkbox colorScheme="pink"/>
-                                        </Td>
-                                        <Td>
-                                            <Box>
-                                                <Text fontWeight="bold">Renato Bessa</Text>
-                                                <Text fontSize="sm" color="gray.300">bessache@outlook.com</Text>
-                                            </Box>
-                                        </Td>
-                                        {isWideVersion && <Td>04 de Abril, 2021</Td>}
-                                        <Td>
-                                            <Button 
-                                                as="a" 
-                                                size="sm" 
-                                                fontSize="sm" 
-                                                colorScheme="purple"
-                                                leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
-                                            >
-                                                {isWideVersion? 'Editar' : ''}
-                                            </Button>
-
-                                        </Td>
-                                    </Tr>
-                                    <Tr>
-                                        <Td px={["4", "4", "6"]}>
-                                            <Checkbox colorScheme="pink"/>
-                                        </Td>
-                                        <Td>
-                                            <Box>
-                                                <Text fontWeight="bold">Renato Bessa</Text>
-                                                <Text fontSize="sm" color="gray.300">bessache@outlook.com</Text>
-                                            </Box>
-                                        </Td>
-                                        {isWideVersion && <Td>04 de Abril, 2021</Td>}
-                                        <Td>
-                                            <Button 
-                                                as="a" 
-                                                size="sm" 
-                                                fontSize="sm" 
-                                                colorScheme="purple"
-                                                leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
-                                            >
-                                                {isWideVersion? 'Editar' : ''}
-                                            </Button>
-                                        </Td>
-                                    </Tr>
+                                    {data.users.map(user=>{
+                                        return (
+                                            <Tr key={user.id}>
+                                                <Td px={["4", "4", "6"]}>
+                                                    <Checkbox colorScheme="pink"/>
+                                                </Td>
+                                                <Td>
+                                                    <Box>
+                                                        <Text fontWeight="bold">{user.name}</Text>
+                                                        <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                                                    </Box>
+                                                </Td>
+                                                {isWideVersion && <Td>{user.createdAt}</Td>}
+                                                <Td>
+                                                    <Button 
+                                                        as="a" 
+                                                        size="sm" 
+                                                        fontSize="sm" 
+                                                        colorScheme="purple"
+                                                        leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
+                                                    >
+                                                        {isWideVersion? 'Editar' : ''}
+                                                    </Button>
+                                                </Td>
+                                            </Tr>
+                                        )
+                                    })}
                                 </Tbody>
                             </Table>
                             <Pagination/>
